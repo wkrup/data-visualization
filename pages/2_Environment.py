ac4a5c77e6165a15e60972313f06410f35381fae
Sehee Kim <tpfpsgoyo@gmail.com>
2023-12-05 17:02:04 +0900

    edit2

--- pages/2_Environment.py
+++ pages/2_Environment.py
@@ -1,28 +1,19 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-import folium 
-import json
-from chart_studio import plotly as py
 
-import os
-
-def fn_energy_use():
+def energy_use():
     energy_html = open('nature_html/energy_page.html', 'r', encoding='utf-8')    
     energy_code = energy_html.read()
-    components.html(energy_code, width=950,height=450)
+    components.html(energy_code, width=950, height=450)
     
 
 def nature_area_1():
-    nature_html = open('nature_html/nature_choropleth.html', 'r', encoding='utf-8')    
+    nature_html = open('nature_html/full_nature_choropleth.html', 'r', encoding='utf-8')    
     nature_code = nature_html.read()
     components.html(nature_code, width=950, height=450)
 
 def nature_area_2():
-    nature_html2 = open('nature_html/nature_bar.html', 'r', encoding='utf-8')
+    nature_html2 = open('nature_html/full_nature_bar.html', 'r', encoding='utf-8')
     nature_code2 = nature_html2.read()
     components.html(nature_code2, width=950, height=450)
 
@@ -40,23 +31,47 @@ def atmos_contam():
 
 st.set_page_config(page_title="Environment")
 st.markdown("# 자연 환경")
-st.sidebar.header("User Settings")
-st.write(
-    """시간의 흐름에 따라 변화하는 서울의 자연 환경을 다양한 측면에서 시각화했습니다."""
+# st.sidebar.header("User Settings")
+
+env_titles = st.sidebar.selectbox(
+    '자연 환경',
+    ['에너지 소비', '녹지 면적', '하수 처리 비용', '대기 오염']
 )
 
-st.markdown("### 에너지 소비")
-fn_energy_use()
 
-st.markdown("### 녹지 면적")
-st.markdown("##### Choropleth")
-nature_area_1()
-st.markdown("##### Bar Graph")
-nature_area_2()
 
-st.markdown("### 하수 처리 비용")
-sewage_cost()
+st.write(
+    """시간의 흐름에 따라 도시화 되어가는 서울의 자연 환경을 다양한 측면에서 분석할 수 있도록 데이터를 시각화했습니다."""
+)
 
-st.markdown("### 대기 오염")
-atmos_contam()
+if env_titles == '에너지 소비':
+    st.markdown("### 에너지 소비")
+    energy_use()
+
+
+if env_titles == '녹지 면적':
+    st.markdown("### 녹지 면적")
+    graph_types = st.radio(
+        "Select graph type",
+        ["Choropleth", "Bar Graph"], 
+        horizontal=True
+    )
+    if graph_types == 'Choropleth':
+        st.caption("*2006년 은평구 데이터는 누락되었기 때문에 Graph에 등장하지 않습니다.")
+        nature_area_1()
+        st.caption("geojson file from https://github.com/southkorea/seoul-maps")
+    else:
+        st.caption("*2006년 은평구 데이터는 누락되었기 때문에 Graph에 등장하지 않습니다.")
+        nature_area_2()
+
+
+
+
+if env_titles == '하수 처리 비용':
+    st.markdown("### 하수 처리 비용")
+    sewage_cost()
+
+if env_titles == '대기 오염':
+    st.markdown("### 대기 오염")
+    atmos_contam()
 
